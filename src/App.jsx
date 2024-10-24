01669311f4b7fc8b647809d3e81a0bbaaa57e5a7
kevin young <you.k2208@gmail.com>
2024-10-24 10:33:27 -0700

    added HomePage FavoritesPage Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
+import FavoritesPage from "./pages/FavoritesPage";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
