a768713660e1980e11b2de80907b70d8f221d66e
kevin young <you.k2208@gmail.com>
2024-10-25 11:37:17 -0700

    click on heart adds recipe to favorites

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,4 +1,5 @@
 import { Heart, HeartPulse, Soup } from "lucide-react";
+import { useState } from "react";
 
 const getTwoValuesFromArray = (arr) => {
   return [arr[0], arr[1]];
@@ -7,9 +8,34 @@ const getTwoValuesFromArray = (arr) => {
 const RecipeCard = ({ recipe }) => {
   const healthLabels = getTwoValuesFromArray(recipe.healthLabels);
 
+  const [isFavorite, setIsFavorite] = useState(
+    localStorage.getItem("favorites")?.includes(recipe.label)
+  );
+
+  const addRecipeToFavorites = () => {
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    const isRecipeAlreadyInFavorites = favorites.some(
+      (fav) => fav.label === recipe.label
+    );
+
+    if (isRecipeAlreadyInFavorites) {
+      favorites = favorites.filter((fav) => fav.label !== recipe.label);
+      setIsFavorite(false);
+    } else {
+      favorites.push(recipe);
+      setIsFavorite(true);
+    }
+
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  };
+
   return (
     <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-2 relative">
-      <a href="#" className="relative h-32">
+      <a
+        href={`https://www.youtube.com/results?search_query=${recipe.label} recipe`}
+        target="_blank"
+        className="relative h-32"
+      >
         <img
           src={recipe.image}
           alt="recipe img"
@@ -18,8 +44,23 @@ const RecipeCard = ({ recipe }) => {
         <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
           <Soup /> {recipe.yield} Servings
         </div>
-        <div className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer">
-          <Heart size={20} className="hover:fill-red-500 hover:text-red-500" />
+
+        <div
+          className="absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault(); // Prevents click from going to youtube
+            addRecipeToFavorites();
+          }}
+        >
+          {!isFavorite && (
+            <Heart
+              size={20}
+              className="hover:fill-red-500 hover:text-red-500"
+            />
+          )}
+          {isFavorite && (
+            <Heart size={20} className="fill-red-500 text-red-500" />
+          )}
         </div>
       </a>
 
@@ -33,9 +74,9 @@ const RecipeCard = ({ recipe }) => {
       </p>
 
       <div className="flex gap-2 mt-auto">
-        {healthLabels.map((label, index) => (
+        {healthLabels.map((label, idx) => (
           <div
-            key={index}
+            key={idx}
             className="flex gap-1 bg-[#d6f497] items-center p-1 rounded-md"
           >
             <HeartPulse size={16} />
